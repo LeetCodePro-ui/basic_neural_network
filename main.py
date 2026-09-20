576d7d5980ca5b183da59bc7466467e5ae2254b6
LeetCodePro-ui <herhoffer.marco@gmail.com>
2026-09-20 12:43:52 +0000

    finished basic neural network

--- main.py
+++ main.py
@@ -5,17 +5,24 @@ import sys
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
-inputs = [1.2,5.1,2.1]
+inputs = [1.9,5,3.1]
 
-weights = np.array(
-        [[3.1, 2.1, 8.7],
-        [1.8,2.5,3],
-        [2.7,2.7,-4]])
+weights = np.random.uniform(-0.5,0.5, size=(3,3))
 
-biases = [3,2,1]
+biases = np.zeros(3)
 
 goal = [0,1,0]
+learning_rate = 0.1
 
-output = sigmoid(np.dot(inputs,weights) + biases)
 
-print(output)
+for i in range(100000):
+    hidden_layer0 = sigmoid(np.dot(inputs,weights) + biases)
+    distance = hidden_layer0 - goal
+    #delta = wie sehr liegt das neuron falsch?
+    delta = distance * hidden_layer0 * (1 - hidden_layer0)
+    weights = weights - learning_rate * np.outer(inputs,delta)
+    biases = biases -learning_rate * delta
+
+
+print(f"Expected outcome : {goal}")
+print(f"Actuall outcome : {hidden_layer0}")
